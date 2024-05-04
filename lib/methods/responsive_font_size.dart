7578defff6eb83c3fd68964ab40507ee19b876e1
HeshamMohamed00 <heshammuh@gmail.com>
2024-05-04 21:05:47 +0300

    add comment in method

--- lib/methods/responsive_font_size.dart
+++ lib/methods/responsive_font_size.dart
@@ -4,18 +4,18 @@ double getRespnsiveFontSize(BuildContext context, {required double fontSize}) {
   double scaleFactor = getScalFactor(context);
   double responsiveFontSize = fontSize * scaleFactor;
 
-  double lowerLimit = fontSize * .8;
-  double upperLimit = fontSize * 1.2;
+  double lowerLimit = fontSize * .8; // min value for font size
+  double upperLimit = fontSize * 1.2; // max value for font size
   return responsiveFontSize.clamp(lowerLimit, upperLimit);
 }
 
 double getScalFactor(BuildContext context) {
   double width = MediaQuery.sizeOf(context).width;
   if (width < 600) {
-    return width / 400;
+    return width / 400; // width for mobile
   } else if (width < 900) {
-    return width / 700;
+    return width / 700; // width for tablet
   } else {
-    return width / 1000;
+    return width / 1000; // width for desktop
   }
 }
